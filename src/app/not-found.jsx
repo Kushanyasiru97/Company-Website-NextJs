d3f342e4de8c9024b402c3f61cbc09b613aa22fe
Kushanyasiru97 <kushanyasiruwalpola@gmail.com>
2024-08-29 15:16:22 +0530

    Add First Commit

---
+++ src/app/not-found.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+const NotFound = () =>{
+    return (
+        <div>
+            <h2>Not Found !!</h2>
+            <p>Sorry, The page you are looking for does nor Exists...</p>
+            <Link href="/">Return Home</Link>
+
+        </div>
+    )
+
+}
+
+    export default NotFound
---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,52 @@
+
+import NavLink from "./navLink/navLink";
+import styles from "./links.module.css";
+
+
+const Links = () => {
+    const links = [
+        {
+            title: "Homepage",
+            path: "/",
+          },
+          {
+            title: "About",
+            path: "/about",
+          },
+          {
+            title: "Contact",
+            path: "/contact",
+          },
+          {
+            title: "Blog",
+            path: "/blog",
+          },
+    ];
+
+    const session = true
+    const isAdmin = true
+
+    return (
+        <div className={styles.link}>
+            {links.map((link =>(
+                <NavLink item={link} key={link.title} />
+            )))}{
+                session ? (
+                    <>
+                    {
+                        isAdmin && (
+                            <NavLink item={{title:"Admin", path:"/admin"}} />
+                        )
+                    }
+                    <button className={styles.logout}>Logout</button>
+
+                    </>
+                ): (
+                    <NavLink item = {{title: "Login", path:"/login"}} />
+                )
+            }
+        </div>
+    )
+  };
+  
+  export default Links;
